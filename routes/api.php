4e2201f22f05dfc1615868fc353b1308bd6ebff0
apple <denis.rozganyaev@gmail.com>
2024-07-29 23:22:27 +0300

    Router, Base controller

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Core\Router;
 // _/users/4/edit
 // _/users/14/edit
 // _/users/114/edit
-Router::get('users/{id:\d+}/edit'); //->controller(UsersController::class)->action('edit');
+Router::get('users/{id:\d+}/notes/{note_id:\d+}')->controller(UsersController::class)->action('edit');
+Router::post('users/store')->controller(UsersController::class)->action('store');
